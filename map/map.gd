5952d2e7942832eefaf3c3fc156c75bc7811ae36
Michael Crapse <mikeeybikeey@gmail.com>
2025-11-15 07:26:59 -0800

    Use doc comments

--- map/map.gd
+++ map/map.gd
@@ -15,23 +15,23 @@ func get_at(pos: Vector2) -> Cell:
 	var idx := xy_to_idx(xy)
 	return cells[idx] if is_inbounds(xy) else null
 
-# Returns the x and y index from the position.
+## Returns the x and y index from the position.
 func pos_to_xy(pos: Vector2) -> Vector2i:
 	return Vector2i(floor(pos.x / CELL_SIZE), floor(pos.y / CELL_SIZE))
 
-# Returns the array index of the x and y cell.
+## Returns the array index of the x and y cell.
 func xy_to_idx(xy: Vector2i) -> int:
 	return xy.x + xy.y * width
 
-# Returns the position of the x and y cell.
+## Returns the position of the x and y cell.
 func xy_to_pos(xy: Vector2i) -> Vector2:
 	return Vector2(xy) * CELL_SIZE + Vector2.ONE * CELL_SIZE / 2.0
 
-# Returns `true` if the x and y is in the bounds of the map.
+## Returns `true` if the x and y is in the bounds of the map.
 func is_inbounds(xy: Vector2i) -> bool:
 	return xy.x >= 0 && xy.x < width && xy.y >= 0 && xy.y < height
 
-# Returns the cell that the ray hits from the starting position to the direction.
+## Returns the cell that the ray hits from the starting position to the direction.
 func castray(start: Vector2, direction: float) -> Vector2:
 	var dir := Vector2i(Vector2.from_angle(direction).ceil())
 	var xy := pos_to_xy(start) + dir

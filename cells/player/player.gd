21ff346405293cf704f27d563fd7a18cc19fd321
Jake Gendreau <jake.lgendreau@gmail.com>
2025-11-15 12:44:58 -0800

    Laser preview

--- cells/player/player.gd
+++ cells/player/player.gd
@@ -1,6 +1,8 @@
 extends "res://cells/base_cell.gd"
 
 var cursor := Vector2.ZERO
+@export var preview_laser_distance: int = 250
+var direction: int
 
 func laser_in(_in_laser: Laser) -> void:
 	GameEvents.game_over.emit()
@@ -12,7 +14,21 @@ func _unhandled_input(event: InputEvent):
 func _input(event: InputEvent):
 	if event is InputEventMouseMotion:
 		cursor = event.position
+		preview_laser_direction()
+		
 
 func shoot():
-	var direction := Laser.rad_to_direction(global_position.direction_to(cursor).angle())
+	#var direction := Laser.rad_to_direction(global_position.direction_to(cursor).angle())
 	laser_out(direction, Color.WHITE, Laser.NORMAL_STRENGTH)
+	
+
+# Automatically draws a line to represent the direction that the laser will go in
+func preview_laser_direction():
+	direction = Laser.rad_to_direction(global_position.direction_to(cursor).angle())
+	queue_redraw()
+
+
+func _draw():
+	var angle := set_direction_in_bounds(direction - 2) * PI / 4.0
+	var dir_vec := Vector2(cos(angle), sin(angle))
+	draw_line(Vector2.ZERO, dir_vec * preview_laser_distance, Color(Color.WHITE, 0.5), 3)
